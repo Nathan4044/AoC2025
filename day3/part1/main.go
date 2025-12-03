d325e59bbf0a36ea62c585f3732a0c6cf33b67e1
Nathan Boswell <nboswell@roku.com>
2025-12-03 08:25:33 +0000

    Day 3 part 1

---
+++ day3/part1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	// f, err := os.Open("test_input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	total := 0
+
+	for scanner.Scan() {
+		str := scanner.Text()
+		line := []byte(str)
+
+		firstPos, first := getByte(line[:len(line)-1])
+		_, second := getByte(line[firstPos+1:])
+
+		result := (int(first) - 48) * 10 + (int(second) - 48)
+		total += result
+	}
+
+	fmt.Println(total)
+}
+
+func getByte(line []byte) (int, byte) {
+	highest := line[0]
+	highestPos := 0
+	for i, c := range []byte(line) {
+		if c > highest {
+			highest = c
+			highestPos = i
+		}
+	}
+
+	return highestPos, highest
+}
