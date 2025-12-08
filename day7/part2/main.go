2f81ec301d23a4bda3d5de9c78ab3a14b274f332
Nathan Boswell <nboswell@roku.com>
2025-12-08 09:24:28 +0000

    Day 7 part 2

---
+++ day7/part2/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	view := parseInput("example_input.txt")
+
+	beams := map[int]int{}
+	beams[bytes.Index(view[0], []byte{'S'})]++
+
+	for _, line := range view[1:] {
+		newBeams := map[int]int{}
+
+		for k, v := range beams {
+			switch line[k] {
+			case '.':
+				newBeams[k] += v
+			case '^':
+				newBeams[k-1] += v
+				newBeams[k+1] += v
+			default:
+				panic("impossible byte")
+			}
+		}
+
+		beams = newBeams
+	}
+
+	total := 0
+	for _, v := range beams {
+		total += v
+	}
+
+	fmt.Println(total)
+}
+
+func parseInput(filename string) [][]byte {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	lines := [][]byte{}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, bytes.Clone(scanner.Bytes()))
+	}
+
+	return lines
+}
